88228743842b1bbb14e4c9293e2854ec26c52768
chenaiquan <like.aiquan@qq.com>
2022-08-26 23:10:15 +0800

    add note

--- easy-rocket-starter/src/main/java/easy/rocket/util/JsonUtil.java
+++ easy-rocket-starter/src/main/java/easy/rocket/util/JsonUtil.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 /**
+ * TODO 这个类应该是全局公用的一个 json util 不应该在这里 new 一个 object mapper
+ *
  * @author chenaiquan
  * @date 2022/6/14 17:05
  */
